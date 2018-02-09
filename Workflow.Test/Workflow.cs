1c8ce38ce39bb4f4eea5edda1a0bb104b5eff81b
BEIJING\jetcli <jetcli@objectivasoftware.com>
2018-02-09 19:06:23 +0800

    init project

---
+++ Workflow.Test/Workflow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Workflow.Test
+{
+    [TestClass]
+    public class Workflow
+    {
+        [TestMethod]
+        public void WorkflowBuildingTest()
+        {
+        }
+    }
+}
